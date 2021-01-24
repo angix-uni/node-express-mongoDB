35ee76fb759975cd3064104bb31536befa5084e1
Angelika <63903851+angix-uni@users.noreply.github.com>
2021-01-24 08:11:45 +0100

    Update author.js

--- api/models/author.js
+++ api/models/author.js
@@ -12,7 +12,7 @@ authorSchema.pre('remove', function(next) {
     if (err) {
       next(err);
     } else if (books.length > 0) {
-      next(new Error('This author has books still'));
+      next(new Error('This author has books'));
     } else {
       next();
     }
